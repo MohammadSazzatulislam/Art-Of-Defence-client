9aec9cdab4c6d43eea680c2a77cb7c75e4fbb8e2
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-11-08 10:52:26 +0600

    add authentication form

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
-import logo from '../../images/logo.jpg'
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.jpg";
 
 const Header = () => {
   const [show, setshow] = useState(false);
@@ -10,7 +10,11 @@ const Header = () => {
         {/* For large and Medium-sized Screen */}
         <div className=" flex justify-between items-center ">
           <div className=" flex space-x-3 items-center">
-            <img className="w-16 p-0 m-0 border rounded-full" src={logo} alt="" />
+            <img
+              className="w-16 p-0 m-0 border rounded-full"
+              src={logo}
+              alt=""
+            />
             <h1 className=" font-semibold text-2xl leading-6 text-gray-800">
               Art Of Defensee
             </h1>
@@ -24,12 +28,16 @@ const Header = () => {
           </div>
 
           <div className="hidden sm:flex flex-row space-x-4">
-            <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-indigo-700 bg-white border border-indigo-700 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
-              Sign Up
-            </button>
-            <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">
-              Sign In
-            </button>
+            <Link to="signup">
+              <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-indigo-700 bg-white border border-indigo-700 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
+                Sign Up
+              </button>
+            </Link>
+            <Link to='/signin'>
+              <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">
+                Sign In
+              </button>
+            </Link>
           </div>
           {/* Burger Icon */}
           <div
@@ -106,12 +114,16 @@ const Header = () => {
             <Link>My Reviews</Link>
           </div>
           <div className="flex flex-col gap-4 mt-4 w-80 mx-auto ">
-            <button className="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-indigo-700 bg-indigo-600 bg-opacity-0 hover:opacity-100 border border-indigo-700 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
-              Sign Up
-            </button>
-            <button className="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">
-              Sign In
-            </button>
+            <Link to="/signup">
+              <button className="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-indigo-700 bg-indigo-600 bg-opacity-0 hover:opacity-100 border border-indigo-700 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
+                Sign Up
+              </button>
+            </Link>
+            <Link to="/signin">
+              <button className="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">
+                Sign In
+              </button>
+            </Link>
           </div>
         </div>
       </nav>
