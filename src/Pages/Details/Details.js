f9511c058239d89e7a08a122eb31b2122231c225
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-11-09 02:57:07 +0600

    add reviews

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { UserContext } from "../../Context/AuthContext/AuthContext";
+import { Link } from "react-router-dom";
 
 const Details = () => {
+  const { user } = useContext(UserContext);
+
   const { name, details, img, price, category } = useLoaderData();
 
   return (
@@ -40,62 +44,108 @@ const Details = () => {
         </div>
       </div>
 
-      {/* another section */}
-      <div>
-        <form>
-          <div className="bg-gray-300 mx-auto w-1/2 mb-5 h-1"></div>
-          <div className="lg:w-4/5 md:w-4/5 w-full p-2 mx-auto gap-3 flex ">
-            <div className="lg:w-1/2 w-full mx-auto">
-              <label className="text-sm leading-none text-gray-800" id="Name">
-                Name
-              </label>
-              <input
-                type="name"
-                tabindex="0"
-                className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
-                aria-labelledby="firstName"
-                placeholder="your Name"
-              />
-            </div>
-            <div className="lg:w-1/2 w-full mx-auto ">
-              <label className="text-sm leading-none text-gray-800" id="email">
-                Your Email
-              </label>
-              <input
-                type="email"
-                readOnly
-                tabindex="0"
-                className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
-                aria-labelledby="lastName"
-                placeholder="Doe"
-              />
-            </div>
-          </div>
-          <div className="lg:w-4/5 md:w-4/5 w-full mx-auto p-2">
-            <label
-              className="text-sm leading-none text-gray-800"
-              id="firstName"
-            >
-              Description
-            </label>
-            <textarea
-              className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
-              name=""
-              id=""
-              cols="30"
-              rows="6"
-              placeholder="start type here..."
-            ></textarea>
-          </div>
-          <div className="lg:w-4/5 md:w-4/5 w-full mx-auto p-2">
-            <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 hover:bg-gray-100 w-full h-full py-3 px-20 bg-blue-100 border rounded border-gray-200">
-              <p className="text-sm font-medium leading-none text-gray-600">
-                Submit
-              </p>
-            </button>
-          </div>
-        </form>
+      {/* review  section */}
+      <div className="flex justify-between items-center p-5">
+        <h1 className="text-md font-bold lg:text-2xl border-l-4 border-blue-500 ">
+          Reviews
+        </h1>
+        {user?.uid ? (
+          <h3 className="font-bold text-yellow-600 pr-5 italic text-md">
+            Add Review
+          </h3>
+        ) : (
+          <Link to="/signin">
+            <h2 className="font-semibold hover:underline">
+              Please login to add a review{" "}
+            </h2>
+          </Link>
+        )}
       </div>
+
+      <div className="grid lg:grid-cols-4 gap-5 p-5 mb-5 md:grid-cols-3 gird-cols-1 mx-auto items-center justify-center ">
+        <div className="w-full flex items-center justify-center">
+          <div className=" w-full hover:shadow-2xl flex flex-col items-center py-8 px-5 bg-gray-100 rounded-md">
+            <div className="w-full flex items-center justify-center">
+              <div className="flex flex-col items-center">
+                <img
+                  src="https://cdn.tuk.dev/assets/templates/olympus/profile.png"
+                  alt="profile"
+                />
+                <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-center">
+                  Ricardo Boveta
+                </p>
+              </div>
+            </div>
+            <div className="mt-3">
+              <p className=" font-semibold ">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam,
+                alias.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* review form section */}
+      {user?.uid && (
+        <div>
+          <form>
+            <div className="bg-gray-300 mx-auto w-1/2 mb-5 h-1"></div>
+            <div className="lg:w-4/5 md:w-4/5 w-full p-2 mx-auto gap-3 flex ">
+              <div className="lg:w-1/2 w-full mx-auto">
+                <label className="text-sm leading-none text-gray-800" id="Name">
+                  Name
+                </label>
+                <input
+                  type="name"
+                  tabindex="0"
+                  className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+                  aria-labelledby="firstName"
+                  placeholder="your Name"
+                />
+              </div>
+              <div className="lg:w-1/2 w-full mx-auto ">
+                <label
+                  className="text-sm leading-none text-gray-800"
+                  id="email"
+                >
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  readOnly
+                  tabindex="0"
+                  className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+                  aria-labelledby="lastName"
+                  placeholder="Doe"
+                />
+              </div>
+            </div>
+            <div className="lg:w-4/5 md:w-4/5 w-full mx-auto p-2">
+              <label
+                className="text-sm leading-none text-gray-800"
+                id="firstName"
+              >
+                Description
+              </label>
+              <textarea
+                className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+                name=""
+                id=""
+                cols="30"
+                rows="6"
+                placeholder="start type here..."
+              ></textarea>
+            </div>
+            <div className="lg:w-4/5 md:w-4/5 w-full mx-auto p-2">
+              <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 hover:bg-gray-100 w-full h-full py-3 px-20 bg-blue-100 border rounded border-gray-200">
+                <p className="text-sm font-medium leading-none text-gray-600">
+                  Submit
+                </p>
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
