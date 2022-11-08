927e6f40caa5ce5790643703df67fd8be15c7174
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-11-09 02:05:00 +0600

    add review from

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -4,7 +4,6 @@ import { useLoaderData } from "react-router-dom";
 const Details = () => {
   const { name, details, img, price, category } = useLoaderData();
 
-
   return (
     <div>
       <div className="2xl:container 2xl:mx-auto lg:py-10 lg:px-20 md:py-5 md:px-3 py-2 px-2 ">
@@ -41,6 +40,62 @@ const Details = () => {
         </div>
       </div>
 
+      {/* another section */}
+      <div>
+        <form>
+          <div className="bg-gray-300 mx-auto w-1/2 mb-5 h-1"></div>
+          <div className="lg:w-4/5 md:w-4/5 w-full p-2 mx-auto gap-3 flex ">
+            <div className="lg:w-1/2 w-full mx-auto">
+              <label className="text-sm leading-none text-gray-800" id="Name">
+                Name
+              </label>
+              <input
+                type="name"
+                tabindex="0"
+                className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+                aria-labelledby="firstName"
+                placeholder="your Name"
+              />
+            </div>
+            <div className="lg:w-1/2 w-full mx-auto ">
+              <label className="text-sm leading-none text-gray-800" id="email">
+                Your Email
+              </label>
+              <input
+                type="email"
+                readOnly
+                tabindex="0"
+                className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+                aria-labelledby="lastName"
+                placeholder="Doe"
+              />
+            </div>
+          </div>
+          <div className="lg:w-4/5 md:w-4/5 w-full mx-auto p-2">
+            <label
+              className="text-sm leading-none text-gray-800"
+              id="firstName"
+            >
+              Description
+            </label>
+            <textarea
+              className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+              name=""
+              id=""
+              cols="30"
+              rows="6"
+              placeholder="start type here..."
+            ></textarea>
+          </div>
+          <div className="lg:w-4/5 md:w-4/5 w-full mx-auto p-2">
+            <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 hover:bg-gray-100 w-full h-full py-3 px-20 bg-blue-100 border rounded border-gray-200">
+              <p className="text-sm font-medium leading-none text-gray-600">
+                Submit
+              </p>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
